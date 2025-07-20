49474cebe82a890e40e0d19a3a9d9f1647c71257
avazbek27 <avazbekovavazbek277@gmail.com>
2025-07-20 23:55:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let text = prompt("String tupeda malumot kriting")
+let number = prompt("Number tupeda malumot kriting")
+let boolean = prompt("Boolean tupeda malumot kriting")
+
+
+let resultt = alert("Siz kiritgan " + text + " so'zi  " + (typeof text) + " typega tegishli" + 
+"\nSiz kiritgan " + number + " so'zi  " + (typeof 123) + " typega tegishli" + 
+"\nSiz kiritgan " + boolean + " so'zi  " + (typeof true) + " typega tegishli");
